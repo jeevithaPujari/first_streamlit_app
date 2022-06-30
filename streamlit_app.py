f22070f3ca428fbc576bdbd04dc8bddd61acbc95
jeevithaPujari <107859523+jeevithaPujari@users.noreply.github.com>
2022-06-30 14:41:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,12 +40,12 @@ try:
        back_from_function=get_fruityvice_data(fruit_choice)
        streamlit.dataframe(back_from_function)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+       my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+       my_cur = my_cnx.cursor()
+       my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+       my_data_row = my_cur.fetchone()
+       streamlit.text("Hello from Snowflake:")
+       streamlit.text(my_data_row)
 
 # don't run anything to past here will be troubleshoot
 streamlit.stop()
